0f3cd326777464befccb5daa85f2a9a21b56a8c1
NaharFreelancer <nahar.opediatech02@gmail.com>
2025-08-18 19:19:32 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+
+// ===================================
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  hamburger.classList.toggle("active");
+});
